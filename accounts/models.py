daa3d4d05940228a2f35a54e0926d784a9d1b8ed
Abhay Pratap Singh <11abhaystark@gmail.com>
2017-09-12 22:31:40 +0530

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    description = models.CharField(max_length=1000, default='')
+    city = models.CharField(max_length=200,default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+    skills = models.CharField(max_length=200, default='')
+    emp_rating = models.FloatField(default=0)
+
+
+    def __str__(self):
+        return self.user.username
+
+class Feedback(models.Model):
+    feedback = models.CharField(max_length=1000, default='')
+    #feedback_time = models.DateTimeField()
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile,sender=User)
